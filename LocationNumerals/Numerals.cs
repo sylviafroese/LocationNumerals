77a596364d26dd1d2b41f3b39300d4fed46c4bbb
Sylvia Louise Froese <slfroese@rrc.ca>
2019-02-11 13:18:09 -0600

    git commit -m "first commit"

---
+++ LocationNumerals/Numerals.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+// Class Numerals
+namespace LocationNumerals
+{
+    class Numerals
+    {
+ 
+        // Method to convert a number between 2^0 and 2^25 to a Location Numeral
+        public String convertIntToLocationNumeral(int intNumber)
+        {
+            String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            int nextPosition;
+            int workingNumber = intNumber;
+            String returnValue = "";
+
+            
+            // Start searching for the closest match to our decimal number in descending order
+            while (workingNumber > 0)
+            {
+                // once we find the decimal number that is one lower than the decimal conversion of 2^x,
+                // we will find the number we can use to start the conversion with.
+
+                // Converting numbers from 2^0 to 2^25 to Location Numerals
+
+                nextPosition = (int)(Math.Log(workingNumber) / Math.Log(2));
+                workingNumber -= (int)Math.Truncate(Math.Pow(2, nextPosition));
+                returnValue = alphabet[nextPosition] + returnValue;
+
+            }
+            
+            return returnValue;
+           
+        }
+
+        public int convertLocationNumeralToInt(String locationNumeral)
+        {
+            String alphabet = "abcdefghijklmnopqrstuvwxyz";
+     
+            int i = 0; // used to loop through the index of location numeral string
+            int power; // used to find the index in the alphabet array
+            int sum = 0; // used to add all the powers of the 2 
+            int baseValue = 2; // the base number of 2
+
+            // loop through the characters in the location numeral
+            while (i < locationNumeral.Length)
+            {
+                // find the index of the location numeral in the alphabet string
+                // using the power x where 2^x
+                power = alphabet.IndexOf(locationNumeral[i]);
+
+                // add this 2^x to a running total which will be used 
+                // to hold the converted decimal number
+                sum += (int) Math.Pow(baseValue, power);
+                i++;
+            }
+
+            return sum;
+        }
+
+        public String convertLocationNumeralToAbbreviated(String locationNumber)
+        {
+            // call the method that converts a Location Numeral to an integer
+            int intValue = convertLocationNumeralToInt(locationNumber);
+
+            // then use the converted integer to convert to an abbreviated Location Number
+            String locationNumeralAbbrev = convertIntToLocationNumeral(intValue); 
+
+            return locationNumeralAbbrev;
+        }
+
+    }
+}
---
+++ LocationNumerals/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace LocationNumerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numerals number = new Numerals();
+            String abbrev;
+            // convert decimal value to Location Numeral
+            int intValue = 9763;
+
+            if (intValue > 33554432 || intValue < 0)
+                Console.WriteLine("Change the whole number to a number between 1 and 33554432 (2^25) ");
+            else if (intValue < 33554432)
+            {
+                abbrev = number.convertIntToLocationNumeral(intValue);
+                Console.WriteLine("The Location Numeral of " + intValue + " is " + abbrev);
+            }
+
+
+            abbrev = "abbc";
+            // convert Location numeral back to decimal value
+            int convertedIntValue = number.convertLocationNumeralToInt(abbrev);
+            
+            Console.WriteLine("\nThe Location numeral of " + convertedIntValue + " is: " +abbrev );
+
+            // Show how the Location numeral can be converted from extended to be abbreviated            
+            String abbreviatedValue = number.convertLocationNumeralToAbbreviated("abbc");
+           
+            Console.WriteLine("\nThe abbreviated value of abbc is " + abbreviatedValue);
+            
+            Console.WriteLine("\n\nProgram done");
+
+            // this prompt is here so that the console window won't close right away after running the program
+            Console.WriteLine("\nPress the Enter key to close the console window");
+            Console.Read();
+        }
+    }
+}
